ffe6fa90e06c6c21bb36a8e4a74e38beddee7396
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-06 22:53:33 +0500

    adding delete button in working form

--- routes/articles.js
+++ routes/articles.js
@@ -22,5 +22,11 @@ router.post('/add', async function (req, res, next) {
   res.redirect("/articles");
 });
 
+// THIS ROUTE WILL DELETE THE ARTICLE AND REDIRECT TO THE ARTICLES PAGE
+router.get('/delete/:id', async function (req, res, next) {
+  let article = await Article.findByIdAndDelete(req.params.id)
+  res.redirect("/articles")
+});
+
 
 module.exports = router;
