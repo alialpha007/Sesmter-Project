5b45aed2f26538e85e3d73e0b8bd04a8c04cd85a
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-06 22:29:50 +0500

    article adding form is working

--- routes/articles.js
+++ routes/articles.js
@@ -9,10 +9,18 @@ router.get('/', async function (req, res, next) {
   res.render("articles/list", { articles });
 });
 
+// THIS ROUTE WILL TAKE US TO THE ARTICLE ADD PAGE
 router.get('/add', async function (req, res, next) {
   res.render("articles/add");
 });
 
+// AFTER ADDING ARTICLE THIS ROUTE WILL TAKE BACK US TO ADMIN PAGE
+// AND STORE DATA IN DB BY post method
+router.post('/add', async function (req, res, next) {
+  let article = new Article(req.body)
+  await article.save()
+  res.redirect("/articles");
+});
 
 
 module.exports = router;
