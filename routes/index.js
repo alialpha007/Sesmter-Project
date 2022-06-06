deba7f350aedee3852e04e5073ae6bf3e378a25e
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-06 20:10:36 +0500

    showing articles in homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Article = require("../models/article")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  let articles = await Article.find()
+  // console.log(articles)
+  res.render('index', { title: 'Bloggy', articles });
 });
 
 module.exports = router;
