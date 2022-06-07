dd6b5a7e5aceeebf7ddfb00477e6e898da230a50
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-07 08:52:17 +0500

    login and signup without authtication

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,29 @@ var router = express.Router();
 var User = require("../models/user")
 
 /* GET users listing. */
+
+//ROUTE FOR REGISTERING NEW USER
 router.get('/register', function (req, res, next) {
   res.render('users/register');
 });
 
-// Registering  New User
+// ROUTE FOR SUBMITTING CREDENTIALS OF A NEW USER
 router.post('/register', async function (req, res, next) {
   let user = new User(req.body)
   await user.save()
   res.redirect('/');
+});
+
+//ROUTE FOR LOGIN USER
+router.get('/login', function (req, res, next) {
+  res.render('users/login');
+});
+
+// ROUTE FOR SUBMITTING CREDENTIALS OF A LOGIN USER
+router.post('/login', async function (req, res, next) {
+  let user = await User.findOne({ email: req.body.email, password: req.body.password })
+  if (!user) return res.redirect("/login")
+  return escape.redirect("/")
 });
 
 module.exports = router;
