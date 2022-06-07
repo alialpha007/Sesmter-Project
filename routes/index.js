a5b045a4a9e72fa6289568fdbbb4db53b1236059
alialpha007 <94750994+alialpha007@users.noreply.github.com>
2022-06-07 06:54:03 +0500

    add faourities route and button in home page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,16 @@ router.get('/', async function (req, res, next) {
   res.render('index', { title: 'Bloggy', articles });
 });
 
+// 
+router.get('/favorities', function (req, res, next) {
+  res.render('favorities');
+});
+
+// THIS ROUTE WILL ADD TO FAVOUITIES THE ARTICLE AND REDIRECT TO THE HOME PAGE
+router.get('/favorities/:id', async function (req, res, next) {
+  let article = await Article.findById(req.params.id)
+  console.log("Add to favoutrities..............")
+  res.redirect("/favorities")
+});
+
 module.exports = router;
